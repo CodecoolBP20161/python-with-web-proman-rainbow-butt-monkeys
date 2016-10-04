e0868bf813324b039ba55e5ce1386a538bcbe989
makaimark <makaimark@gmail.com>
2016-10-04 13:42:32 +0200

    OOP create box methods and basic storage classes

--- static/index_js.js
+++ static/index_js.js
@@ -2,26 +2,56 @@
  * Created by makaimark on 2016.10.03..
  */
 
-function handle_new_board_name() {
-    $('#myModal').modal('toggle');
-
-    var x = document.getElementById("new_board").value;
-    document.getElementById("new_board").value = '';
-
-    var div = document.createElement("div");
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.background = "red";
-    div.style.color = "white";
-    div.innerHTML = "Hello new board: " + x;
-    div.setAttribute('id', 'draggable');
-    document.body.appendChild(div);
-}
-function new_board(name){
+function Board(name){
     this.name = name;
 }
 
+var myStorage = function (){
+    this.boards = [];
+};
+
+//noinspection JSAnnotator
+myStorage.saveBoard() = {};
+//noinspection JSAnnotator
+myStorage.getBoard() = {};
+//noinspection JSAnnotator
+myStorage.implement() = {};
+
+
+myLocalStorage.saveBoard = function() {
+
+ };
+
+function handleNewBoardName() {
+    $('#myModal').modal('toggle');
+
+    var name = document.getElementById("new_board").value;
+    var new_board = new Board(name);
+    document.getElementById("new_board").value = '';
+    return new_board;
+}
+
+function displayBoard(board) {
+    var div = document.createElement("div");
+    var header = document.createElement('h3');
+    div.innerHTML = "Hello new board: " + board.name;
+    div.setAttribute('class', 'board');
+
+    $(".container-custom").append(div);
+
+}
+
+function eventHandler(){
+    var new_board = handleNewBoardName();
+    displayBoard(new_board);
+}
+
+//var boards = [];
+
+//JSON.parse(json_str);
+
+//JSON.stringify(value[, replacer[, space]]);
+
 $(document).ready(function() {
-    $("#save_board_button").click(handle_new_board_name);
-    $("#draggable").draggable();
+    $("#save_board_button").click(eventHandler);
 });
