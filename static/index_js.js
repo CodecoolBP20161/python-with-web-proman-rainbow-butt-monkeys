99d0702f89396af2ea7bd5b388d463645abee5e6
makaimark <makaimark@gmail.com>
2016-10-05 14:06:51 +0200

    OOP boards class

--- static/index_js.js
+++ static/index_js.js
@@ -2,53 +2,55 @@
  * Created by makaimark on 2016.10.03..
  */
 
+
+var Boards = function () {
+    var self = this;
+
+    this.Board = function (name) {
+        this.name = name;
+        var date = new Date();
+        this.id = date.getTime();
+    };
+    this.handleNewBoardName = function () {
+        $('#myModal').modal('toggle');
+        var name = document.getElementById("new_board").value;
+        var new_board = new self.Board(name);
+        document.getElementById("new_board").value = '';
+        return new_board;
+    };
+    this.displayBoard = function (board){
+        var div = document.createElement("div");
+        div.innerHTML = "Hello new board: " + board.name + " " + board.id;
+        div.setAttribute('class', 'board');
+        $(".container-custom").append(div);
+    };
+    this.boardLister = function () {
+        var boards = mystorage.getBoards();
+        if ( boards != null ) {
+            for (var i = 0; i < boards.length; i++) {
+                self.displayBoard(boards[i]);
+            }
+        }
+    };
+    this.saveBoardClickEventHandler = function () {
+         $(".container-custom").html("");
+        var new_board = self.handleNewBoardName();
+        console.log(new_board);
+        mystorage.saveBoard(new_board);
+        self.boardLister();
+    }
+};
+
+var boards = new Boards();
 var mystorage = new myStorage( new myLocalStorage());
 
-function Board(name){
-    this.name = name;
-    var date = new Date();
-    this.id = date.getTime();
-}
 
 function Card(name, board_id){
     this.name = name;
     this.id = board_id;
 }
 
-function handleNewBoardName() {
-    $('#myModal').modal('toggle');
-    var name = document.getElementById("new_board").value;
-    var new_board = new Board(name);
-    document.getElementById("new_board").value = '';
-    return new_board;
-}
-
-function displayBoard(board) {
-    var div = document.createElement("div");
-    div.innerHTML = "Hello new board: " + board.name + " " + board.id;
-    div.setAttribute('class', 'board');
-    $(".container-custom").append(div);
-}
-
-function boardLister() {
-    var boards = mystorage.getBoards();
-    console.log(boards);
-    if ( boards != null ) {
-        for (var i = 0; i < boards.length; i++) {
-            displayBoard(boards[i]);
-        }
-    }
-}
-
-function saveBoardClickEventHandler(){
-    $(".container-custom").html("");
-    var new_board = handleNewBoardName();
-    console.log(new_board);
-    mystorage.saveBoard(new_board);
-    boardLister();
-}
-
 $(document).ready(function() {
-    boardLister();
-    $("#save_board_button").click(saveBoardClickEventHandler);
+    boards.boardLister();
+    $("#save_board_button").click(boards.saveBoardClickEventHandler);
 });
